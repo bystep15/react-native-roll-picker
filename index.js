1625d070dbd9c2e6e90dcc127799861299f6168b
胡继伟 <hu_jiwei@foxmail.com>
2018-02-13 19:45:11 +0800

    注释冗余代码

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * Created by yjy on 2016/12/26.
  */
 const Picker = require('./lib/Picker');
-const PickerSpace = require('./lib/PickerSpace');
-const CityPicker = require('./demo/CityPicker');
+// const PickerSpace = require('./lib/PickerSpace');
+// const CityPicker = require('./demo/CityPicker');
 
-module.exports = {Picker, PickerSpace, CityPicker};
+// module.exports = {Picker, PickerSpace, CityPicker};
 module.exports = Picker;
